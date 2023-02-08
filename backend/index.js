666244daeb66d54f304980506fb584542ccaecb7
Adrian Pearman <adrianpearman12@gmail.com>
2023-02-08 15:45:56 -0500

    testing

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,20 @@ const PORT = process.env.PORT || 9000
 const clientRoot = path.join(__dirname, "../client", "build")
 
 app.get('/', (req, res) => {
-    // res.send("HELLO")
+    res.send({
+        hello: "hello"
+    })
     app.use(express.static(clientRoot))
-    res.sendFile(clientRoot, "index.html")
+    // res.sendFile(clientRoot, "index.html")
 })
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(clientRoot))
-    app.get('/', (req, res) => {
-        // res.sendFile("index.html", {clientRoot})
-        res.send({ hello: "jhgsvkhg"})
-    })
-}
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(clientRoot))
+//     app.get('/', (req, res) => {
+//         // res.sendFile("index.html", {clientRoot})
+//         res.send({ hello: "jhgsvkhg"})
+//     })
+// }
 
 app.listen(PORT, () => {
     console.log(`App is running on PORT:${PORT}`)
